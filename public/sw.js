b572db32d6f06b03c84fdf10b1586dc112de2134
CHINTU628 <chintukiffi@gmail.com>
2026-02-13 19:54:58 +0530

    Refactor push notification handling in sw.js
    Updated notification handling to use a new URL for user page.

--- public/sw.js
+++ public/sw.js
@@ -1,22 +1,33 @@
 self.addEventListener("push", event => {
-
   const data = event.data.json();
 
   self.registration.showNotification(data.title, {
     body: data.body,
-    icon: data.icon || "https://cdn-icons-png.flaticon.com/512/1827/1827392.png",
+    icon: data.icon,
     image: data.image,
-    badge: data.icon,   // small monochrome icon (Android style)
+
     data: {
-      url: "http://localhost:3000/user.html"
+      url: "https://push-notification-system-chof.onrender.com/user.html"
     }
   });
-
 });
 
-self.addEventListener("notificationclick", function(event) {
+self.addEventListener("notificationclick", event => {
   event.notification.close();
+
   event.waitUntil(
-    clients.openWindow(event.notification.data.url)
+    clients.matchAll({ type: "window", includeUncontrolled: true })
+      .then(clientList => {
+
+        for (const client of clientList) {
+          if (client.url === event.notification.data.url && "focus" in client) {
+            return client.focus();
+          }
+        }
+
+        if (clients.openWindow) {
+          return clients.openWindow(event.notification.data.url);
+        }
+      })
   );
 });
